38a2470586e2765d4ab7908166a80f20c3d1083f
BS <bohdan.strilets@gmail.com>
2026-05-11 10:17:08 +0200

    feat(user): connect userModule to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { CryptoModule } from '@common/crypto';
 import { TokensModule } from '@modules/tokens';
+import { UsersModule } from '@modules/users';
 import { Module } from '@nestjs/common';
 
 import { AppConfigModule } from './config';
 import { PrismaModule } from './prisma';
 
 @Module({
-  imports: [AppConfigModule, PrismaModule, CryptoModule, TokensModule],
+  imports: [AppConfigModule, PrismaModule, CryptoModule, TokensModule, UsersModule],
 })
 export class AppModule {}
