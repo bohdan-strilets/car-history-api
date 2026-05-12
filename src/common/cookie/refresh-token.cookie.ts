c1142a98aacf3303bccd416fd0eb1aa87a648572
BS <bohdan.strilets@gmail.com>
2026-05-12 13:42:19 +0200

    feat(cookie): add utils for management refresh cookies

---
+++ src/common/cookie/refresh-token.cookie.ts
@@ -0,0 +1,33 @@
+import { AppConfigService } from '@config/config.service';
+import { Request, Response } from 'express';
+import ms from 'ms';
+
+import { REFRESH_TOKEN_COOKIE } from './constants.cookie';
+
+const getApiPrefix = (config: AppConfigService): string => {
+  return `/${config.prefix}/auth`;
+};
+
+export const setRefreshTokenCookie = (
+  res: Response,
+  token: string,
+  config: AppConfigService,
+): void => {
+  res.cookie(REFRESH_TOKEN_COOKIE, token, {
+    httpOnly: true,
+    secure: config.isProduction,
+    sameSite: 'strict',
+    maxAge: ms(config.jwtRefreshExpiresIn),
+    path: getApiPrefix(config),
+  });
+};
+
+export const clearRefreshTokenCookie = (res: Response, config: AppConfigService): void => {
+  res.clearCookie(REFRESH_TOKEN_COOKIE, {
+    path: getApiPrefix(config),
+  });
+};
+
+export const getRefreshTokenFromCookie = (req: Request): string | null => {
+  return req.cookies[REFRESH_TOKEN_COOKIE] || null;
+};
