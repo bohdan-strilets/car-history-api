1f0d8fa29ae6e144094b6450e4736b67190eab37
BS <bohdan.strilets@gmail.com>
2026-05-12 13:44:36 +0200

    feat(device): add parser for user device and ip

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,2 +1,4 @@
 export { createExpiresAt } from './create-expires-at';
 export { formatEmail } from './format-email';
+export { parseDeviceName } from './parse-device-name';
+export { parseIpAddress } from './parse-ip-address';
